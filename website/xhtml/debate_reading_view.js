afd7d1d320c16a23e10dac0ca176a19ad940c253
zme1 <zme1@pitt.edu>
2017-04-19 21:33:40 -0400

    continued attempts to add interactivity to the debate reading pages with Javascript

--- website/xhtml/debate_reading_view.js
+++ website/xhtml/debate_reading_view.js
@@ -1,11 +1,14 @@
 window.addEventListener('DOMContentLoaded', init, false);
 
-function init(){
-var buttons = document.getElementsByName('button')
-var button = buttons[0]
-button.addEventListener('click', show_hide, false);
+function init() {
+    var buttons = document.getElementsByTagName('button');
+    var button = buttons[0];
+    button.addEventListener('click', show_hide, false);
 }
 
-function show_hide(){
-    console.log('success!')
+function show_hide() {
+    var participants = document.getElementsByClassName('containerMeta');
+    if (participants.style.display == 'flex') {
+        alert('HEY');
+    }
 }
